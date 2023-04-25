bef8d92037b85c509c51b3c2e4e2ea84eb22bd23
Andrew Leonard <anleonar@redhat.com>
2023-04-25 23:07:09 +0100

    Force compiler jck tests to 1500m heap
    Signed-off-by: Andrew Leonard <anleonar@redhat.com>

--- jck/jtrunner/JavaTestRunner.java
+++ jck/jtrunner/JavaTestRunner.java
@@ -828,7 +828,7 @@ public class JavaTestRunner {
 			} else if (jckVersion.contains("jck11")) {
 				fileContent += "set jck.env.compiler.testCompile.otherOpts \"-source 11 \"" + ";\n";
 			} else { // This is the case where JCK Version > 11
-				fileContent += "set jck.env.compiler.testCompile.otherOpts \"-Xmx1500m -source " + jckVersionNo + " --enable-preview\"" + ";\n";
+				fileContent += "set jck.env.compiler.testCompile.otherOpts \" -J-Xmx1500m -source " + jckVersionNo + " --enable-preview\"" + ";\n";
 			} 
 
 			if (tests.contains("api/java_rmi") || tests.equals("api")) {
