1bfaa4432cf097ed6467cb0595916177bfac0d4b
Andrew Leonard <andrew.m.leonard@ibm.com>
2026-02-13 09:48:45 +0000

    Arctic ShadeManager fix
    Signed-off-by: Andrew Leonard <andrew.m.leonard@ibm.com>

--- jck/interactives/arctic_tests/runTests.sh
+++ jck/interactives/arctic_tests/runTests.sh
@@ -193,12 +193,12 @@ if [ $SPEC = "linux_ppc-64_le" ]; then
 elif [ $SPEC = "linux_390-64" ]; then
     wget -q https://ci.adoptium.net/job/Build_Arctic_s390x_linux/lastSuccessfulBuild/artifact/upload/arctic-0.8.1.jar
 else
-    echo "Getting https://ci.adoptium.net/job/UploadFile/129/artifact/upload/arctic-0.8.4.jar"
-    wget -q https://ci.adoptium.net/job/UploadFile/129/artifact/upload/arctic-0.8.4.jar
-ls -l arctic-0.8.4.jar
+    echo "Getting https://ci.adoptium.net/job/UploadFile/130/artifact/upload/arctic-0.8.5.jar"
+    wget -q https://ci.adoptium.net/job/UploadFile/130/artifact/upload/arctic-0.8.5.jar
+ls -l arctic-0.8.5.jar
 fi
 
-mv arctic-0.8.4.jar ${LIB_DIR}/arctic.jar
+mv arctic-0.8.5.jar ${LIB_DIR}/arctic.jar
 ls -l ${LIB_DIR}
 
 cp $JENKINS_HOME_DIR/jck_run/arctic/$OSNAME/player.properties .
